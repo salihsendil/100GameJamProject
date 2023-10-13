97457cca7b230431e412d07f3dbef3dd508c0926
salihsendil <sendillsalih@gmail.com>
2023-10-14 02:13:57 +0300

    enemy animation and pathinding updated

--- 100GameJamProject/Assets/Scripts/Pathfinder.cs
+++ 100GameJamProject/Assets/Scripts/Pathfinder.cs
@@ -7,11 +7,22 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] GameObject path;
     List<Transform> waypoints;
     int waypointIndex = 0;
-    float moveSpeed = 5f;
+    float moveSpeed = 3f;
+    Rigidbody2D rb;
+    Animator animator;
+    int animHorizontalHash;
+    int animVerticalHash;
+    int animIsWalking;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animHorizontalHash = Animator.StringToHash("Horizontal");
+        animVerticalHash = Animator.StringToHash("Vertical");
+        animIsWalking = Animator.StringToHash("isWalking");
+
         waypoints = new List<Transform>();
         Debug.Log(path.transform.childCount);
         for (int i = 0; i < path.transform.childCount; i++) {
@@ -30,6 +41,7 @@ public class Pathfinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 previousPos = transform.position;
         if (transform.position == waypoints[waypointIndex].position) {
             if (waypoints.Capacity - 1 == waypointIndex) {
                 waypoints.Reverse();
@@ -38,7 +50,18 @@ public class Pathfinder : MonoBehaviour
             waypointIndex++;
         }
         transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, moveSpeed * Time.deltaTime);
-        //transform.LookAt(Vector3.forward, transform.right);
+        Vector2 currentPos = transform.position;
+        rb.velocity = previousPos - currentPos;
+        animator.SetFloat(animHorizontalHash, rb.velocity.y);
+        animator.SetFloat(animVerticalHash, rb.velocity.x);
+        animator.SetBool(animIsWalking, rb.velocity != Vector2.zero);
+        Debug.Log(rb.velocity);
+
+        //rb.MovePosition(waypoints[waypointIndex].position);
+
+        //transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, moveSpeed * Time.deltaTime);
+        //Vector3 pos = new Vector3(0, 0, waypoints[waypointIndex].position.x);
+        //transform.rotation = Quaternion.Euler(pos);
         //transform.rotation = Quaternion.LookRotation(waypoints[waypointIndex].position, Vector2.up);
     }
 }
