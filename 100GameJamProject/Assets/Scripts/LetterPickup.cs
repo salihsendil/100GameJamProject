3f711b3f8d7fb0485e719af87e464c3998eb7c7d
salihsendil <sendillsalih@gmail.com>
2023-10-24 02:48:07 +0300

    lights added home tutorial level developed

--- 100GameJamProject/Assets/Scripts/LetterPickup.cs
+++ 100GameJamProject/Assets/Scripts/LetterPickup.cs
@@ -10,21 +10,24 @@ public class LetterPickup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        wasLetterCollected = true;
-        FindObjectOfType<GameSession>().UpdateLetterAndText();
-        AudioSource.PlayClipAtPoint(letterSound, Camera.main.transform.position, 0.5f);
-        gameObject.SetActive(false);
-        Destroy(gameObject);
+        if (other.isTrigger) {
+            wasLetterCollected = true;
+            FindObjectOfType<GameSession>().UpdateLetterAndText();
+            AudioSource.PlayClipAtPoint(letterSound, Camera.main.transform.position, 0.5f);
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+
     }
 }
